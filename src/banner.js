e9e9886a5dcc30b91939758518f3672a7d83dbd6
Jonas Mitchell <Jonas Mitchell>
2016-10-16 01:06:51 -0400

    added back check for 0 for nav button

--- src/banner.js
+++ src/banner.js
@@ -17,7 +17,8 @@ banner.controller = function() {
   return {
     toggleSideNav: function() {
       // console.log("toggleSideNav");
-      if (document.getElementById("mySidenav").style.width == "0px") {
+      if (document.getElementById("mySidenav").style.width == "0px" || 
+          document.getElementById("mySidenav").style.width == 0) {
            document.getElementById("mySidenav").style.width = "150px";
            document.getElementById("main").style.marginLeft = "150px";
        } else {
